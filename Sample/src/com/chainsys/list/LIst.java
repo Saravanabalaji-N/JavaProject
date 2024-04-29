e69888032cd795f4f3bb901c9d0f4728b97669a7
Saravanabalaji-N <165891285+Saravanabalaji-N@users.noreply.github.com>
2024-04-29 17:50:09 +0530

    Update LIst.java

--- Sample/src/com/chainsys/list/LIst.java
+++ Sample/src/com/chainsys/list/LIst.java
@@ -3,7 +3,40 @@ package com.chainsys.list;
 public class LIst {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(2, 4);
+		System.out.println("The numbers are: " + numbers);
+
+		ArrayList<String> names = new ArrayList<>();
+		names.add("saravana");
+		names.add("naveen");
+		System.out.println("The numbers are: " + names);
+		names.clear();
+		names.add("akash");
+		System.out.println("The numbers are: " + names);
+		
+		Queue<String> queue = new LinkedList<>();
+		queue.offer("Saravana");
+		queue.offer("Naveen");
+		queue.offer("Balaji");
+		System.out.println("poll :"+queue.poll());
+		System.out.println("peek :"+queue.peek());
+		queue.add("Akash");
+		queue.remove("Akash");
+		System.out.println(queue);
+
+		Set<String> Set = new HashSet<String>();
+		Set.add("Saravana");
+		Set.add("Balaji");
+		Set.add("Student");
+		Set.add("Charan");
+		Set.add("Akash");
+		Set.remove("Charan");
+
+		System.out.println(Set);
 
 	}
 
